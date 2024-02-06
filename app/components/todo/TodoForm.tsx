7046a97c1ebde43f403540c776fad354876c0ed1
trungdung70199 <108038744+trungdung70199@users.noreply.github.com>
2024-02-06 12:21:34 +0900

    update

--- app/components/todo/TodoForm.tsx
+++ app/components/todo/TodoForm.tsx
@@ -1,45 +1,3 @@
-// "use client"
-
-// import { useState } from "react";
-// import ClickButton from "@/app/components/ClickButton";
-// import Input from "@/app/components/Input";
-// import TagsInput from "@/app/components/tag/TagsInput";
-
-// interface TodoFormProps {
-//     onSaveTodo: (value: string, tags: string[]) => void;
-//     autoCompleteTags: string[]
-// }
-
-// const TodoForm = ({
-//     onSaveTodo,
-//     autoCompleteTags = []
-// }: TodoFormProps) => {
-//     const [inputValue, setInputValue] = useState('');
-//     const [tags, setTags] = useState<string[]>([]);
-
-//     const addClickHandler = () => {
-//         onSaveTodo(inputValue, tags);
-//         setInputValue("");
-//         setTags([]);
-//     }
-
-//     return (
-//         <div>
-//             <Input
-//                 value={inputValue}
-//                 onChange={setInputValue}
-//                 placeholder="Enter Todo..." />
-
-//             <ClickButton
-//                 label="Add"
-//                 onClick={addClickHandler}
-//                 disabled={!inputValue} />
-//         </div>
-//     );
-// }
-
-// export default TodoForm;
-
 import { useState } from "react";
 import ClickButton from "@/app/components/ClickButton";
 import Input from "@/app/components/Input";
@@ -50,57 +8,40 @@ interface TodoFormProps {
     autoCompleteTags: string[];
 }
 
-const TodoForm = ({ onSaveTodo, autoCompleteTags = [] }: TodoFormProps) => {
-    const [inputValue, setInputValue] = useState("");
+const TodoForm = ({
+    onSaveTodo,
+    autoCompleteTags = []
+}: TodoFormProps) => {
+    const [inputValue, setInputValue] = useState('');
     const [tags, setTags] = useState<string[]>([]);
-    const [todos, setTodos] = useState<string[]>([]);
 
     const addClickHandler = () => {
         onSaveTodo(inputValue, tags);
-        setTodos([...todos, inputValue]); 
         setInputValue("");
         setTags([]);
-    };
-
-    const handleDeleteClick = (index: number) => {
-        const newTodos = [...todos];
-        newTodos.splice(index, 1);
-        setTodos(newTodos);
-    };
+    }
 
     return (
         <div>
             <Input
                 value={inputValue}
                 onChange={setInputValue}
-                placeholder="Enter Todo..."
-            />
+                placeholder="Enter Todo..." />
+
+            {/* TagsInput を追加 */}
             <TagsInput
                 tags={tags}
-                autoCompleteTags={autoCompleteTags}
                 onChange={setTags}
+                autoCompleteTags={autoCompleteTags}
+                placeholder="Enter Tags..."
             />
+
             <ClickButton
                 label="Add"
                 onClick={addClickHandler}
-                disabled={!inputValue}
-            />
-
-            <div>
-                <h2>Added Todos:</h2>
-                <ul>
-                    {todos.map((todo, index) => (
-                        <li key={index}>
-                            {todo}
-                            <button onClick={() => handleDeleteClick(index)}>
-                                Delete
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                disabled={!inputValue} />
         </div>
     );
-};
+}
 
 export default TodoForm;
